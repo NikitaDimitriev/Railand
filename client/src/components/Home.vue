27dfa07df59ec1b4be7cc76c1181f7c336d08e37
nikita <nikitadimitriev000@gmail.com>
2018-10-11 01:02:44 +0300

    add select languages

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
     <Menu></Menu>
-    <p>asd</p>
-    <p>{{msg}}</p>
+
   </div>
 </template>
 
--- client/src/components/shared/Menu.vue
+++ client/src/components/shared/Menu.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="header">
         <div class="top-header">
+          <div class="top-header-left">
             <el-select v-model="phoneNumber">
                 <el-option
                     v-for="item in options"
@@ -9,9 +10,21 @@
                     :value="item.value">
                 </el-option>
             </el-select>
-            <a href="viber://pa?chatURI=+66848422111"><i class="fa fa-whatsapp icon"></i></a>
-            <a href=""><i class="fa fa-volume-control-phone"></i></a>
+            <a href="whatsapp://send?text=!&phone=+66848422111"><i class="fa fa-whatsapp icon"></i></a>
+            <a href="viber://pa?chatURI=+66848422111"><i class="fa fa-volume-control-phone icon"></i></a>
             <a href=""><i class="fa fa-telegram icon"></i></a>
+          </div>
+          <div class="top-header-right">
+            <el-select v-model="language">
+                <el-option
+                    v-for="lang in languages"
+                    :key="lang.value"
+                    :label="lang.img"
+                    :value="lang.value">
+                    <img :src="lang.img" class="language-image">
+                </el-option>
+            </el-select>
+          </div>
         </div>
     </div>
 </template>
@@ -30,22 +43,30 @@ export default {
           label: "+66 84 84 55 111"
         }
       ],
-      phoneNumber: '+66 84 84 22 111'
+      languages:[
+        {name:"russian", img: "../../../static/Flag_of_Russia.svg"}
+      ],
+      phoneNumber: '+66 84 84 22 111',
+      language: 'russian'
     };
   }
 };
 </script>
 <style>
 .header {
-  width: 100%;
   margin: 10px 125px 10px 125px;
+  border-bottom: 1px solid #e5e5e5;
 }
 .top-header{
-    width: 30%;
     display: flex;
     justify-content: space-between;
 }
 .icon {
-  color: #000000;
+  font-size: 24px;
+  color: #bdbdbd;
+}
+.language-image{
+  height: 24px;
+  width: 24px;
 }
 </style>
